bf235d591030fcba4ca7e39e37aebada8ba589c0
Rafael Ferreira <rafaelfferreira99@hotmail.com>
2024-08-20 07:39:41 -0300

    Projeto base

---
+++ src/main/java/Fasters/troia/TroiaApplication.java
@@ -0,0 +1,13 @@
+package Fasters.troia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TroiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TroiaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Fasters/troia/TroiaApplicationTests.java
@@ -0,0 +1,13 @@
+package Fasters.troia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TroiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
